b6c626769788adbca1e03e7b633064eb34b054c0
Hugo Andrade <hugoalvesandrade@hotmail.com>
2024-07-09 21:27:23 -0300

    Salvando ajustes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
@@ -15,6 +16,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Users
+    Route::get('/users-index', [UserController::class, 'index'])->name('user.index');
+    Route::get('/user-edit/{id}', [UserController::class, 'edit'])->name('user.edit');
+    Route::put('/edit-update/{id}', [UserController::class, 'update'])->name('user.update');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
